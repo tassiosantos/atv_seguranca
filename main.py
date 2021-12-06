a6d096980213955e650a9e679ab7e4a4de202303
Tássio Nascimento Santos <tassiosantos@gmail.com>
2021-12-06 10:55:29 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from os import *
+from datetime import date
+
+def iniciar(__self__):
+    
+
+
+if __name__ == '__main__':
+    iniciar()
+    
+    continuar = True
+    
+    while(continuar):
+        try:
+            
